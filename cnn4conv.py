8e45b3491b8c98c627c854cff4f22c65011655ee
unknown <LinhPham123>
2019-12-21 06:45:49 +0000

    done building cnn, need to put segements into same file for accuracy calculation

--- cnn4conv.py
+++ cnn4conv.py
@@ -1,11 +1,11 @@
+import torch
 from torch import nn
 
 class CNN(nn.Module):
-    def __init__(self, height: int, width: int, channels: int, class_count: int, dropout: float):
+    def __init__(self, channels: int, class_count: int, dropout: float):
         super(CNN, self).__init__()
-        self.input_shape = ImageShape(height=height, width=width, channels=channels)
-        self.class_count = class_count
 
+        self.class_count = class_count
         self.drop_out = nn.Dropout(dropout)
 
         
@@ -14,6 +14,7 @@ class CNN(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU()
         )
+        self.initialise_layer(self.layer1)
        
         self.layer2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), stride=(2,2)),
@@ -22,12 +23,14 @@ class CNN(nn.Module):
             nn.MaxPool2d(kernel_size=(2,2)),
             nn.Dropout(0.5)
         )
+        self.initialise_layer(self.layer2)
        
         self.layer3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(2,2)),
             nn.BatchNorm2d(64),
             nn.ReLU
         )
+        self.initialise_layer(self.layer3)
 
         self.layer4 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(2,2)),
@@ -35,40 +38,26 @@ class CNN(nn.Module):
             nn.ReLU,
             nn.Dropout(0.5)
         )
+        self.initialise_layer(self.layer4)
 
-        self.layer4 = nn.Sequential(
+        self.layer5 = nn.Sequential(
             nn.Linear(15488, 1024),
             nn.Sigmoid(),
             nn.Dropout(0.5)
         )
+        self.initialise_layer(self.layer5)
         
-        self.out = nn.Sequential(
-            nn.Linear(1024, 10),
-            nn.Softmax
-        )
+        self.out = nn.Linear(1024, 10)
+        self.initialise_layer(self.out)
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(images)
-        x = self.bn1(x)
-        x = F.relu(x)
-        x = self.pool1(x)
-        ## TASK 2-2: Pass x through the second convolutional layer
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = F.relu(x)
-        ## TASK 3-2: Pass x through the second pooling layer
-        x = self.pool2(x)
-        ## TASK 4: Flatten the output of the pooling layer so it is of shape
-        ##         (batch_size, 4096)
+        x = self.layer1(images)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
         x = torch.flatten(x, start_dim=1)
-        x = self.drop_out(x)
-        ## TASK 5-2: Pass x through the first fully connected layer
-        x = self.fc1(x)
-        x = self.bn3(x)
-        x = F.relu(x)
-        x = self.drop_out(x)
-        ## TASK 6-2: Pass x through the last fully connected layer
-        x = self.fc2(x)
+        x = self.layer5(x)
+        x = self.out(x)
         return x
 
     @staticmethod
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from dataset import UrbanSound8KDataset
-import cnn4conv
+from dataset import *
+from cnn4conv import *
 
+import time
 import argparse
 import torch
+import torch.backends.cudnn
 from torch.utils.data import DataLoader
+from typing import Union, NamedTuple
+
+
+torch.backends.cudnn.benchmark = True
 
 parser = argparse.ArgumentParser(
     description="Train a Two-Stream CNN on Urbansound8K",
@@ -21,25 +27,116 @@ def main(args):
     train_loader = torch.utils.data.DataLoader( 
         UrbanSound8KDataset('UrbanSound8K_train.pkl', 'LMC'), 
         batch_size=32, shuffle=True, 
-        num_workers=8, pin_memory=True) 
+        num_workers=8, pin_memory=True)
  
     val_loader = torch.utils.data.DataLoader( 
-        UrbanSound8KDataset('UrbanSound8K_test.pkl', 'MC'), 
+        UrbanSound8KDataset('UrbanSound8K_test.pkl', 'LMC'), 
         batch_size=32, shuffle=False, 
         num_workers=8, pin_memory=True) 
 
- 
+    model = CNN(1, 10, 0.5)
+    criterion = nn.Softmax()
+    model.to(DEVICE)
+    val_frequency = 2
+    step = 0
+    print_frequency = 20
 
-# for i, (input, target, filename) in enumerate(train_loader): 
-# #           training code
-#     print("hello")
+    for epoch in range(0, 50):
+        model.train()
 
-# for i, (input, target, filename) in enumerate(val_loader): 
-#     print("hi")
-# #           validation code 
+        data_load_start_time = time.time()
+        for i, (batch, target, filename) in enumerate(train_loader): 
+            batch = batch.to(DEVICE)
+            target = target.to(DEVICE)
+            data_load_end_time = time.time()
 
- 
+            logits = model.forward(batch)
+            loss = criterion(logits, target)
+            loss.backward()
 
+            with torch.no_grad():
+                preds = logits.argmax(-1)
+                accuracy = compute_accuracy(target, preds)
+
+            data_load_time = data_load_end_time - data_load_start_time
+            step_time = time.time() - data_load_end_time
+
+            if ((step + 1) % print_frequency) == 0:
+                print_metrics(step, epoch, accuracy, loss, data_load_time, step_time, train_loader)
+            step += 1
+            data_load_start_time = time.time()
+
+        if ((epoch + 1) % val_frequency) == 0:
+            results = {"preds": [], "labels": []}
+            total_loss = 0
+            model.eval()
+
+            with torch.no_grad():
+                for batch, target in val_loader:
+                    batch = batch.to(DEVICE)
+                    target = target.to(DEVICE)
+                    logits = model(batch)
+                    loss = criterion(logits, target)
+                    total_loss += loss.item()
+                    preds = logits.argmax(dim=-1).cpu().numpy()
+                    results["preds"].extend(list(preds))
+                    results["labels"].extend(list(target.cpu().numpy()))
+
+            accuracy = compute_accuracy(
+                np.array(results["labels"]), np.array(results["preds"])
+            )
+            average_loss = total_loss / len(val_loader)
+
+            class_accuracy = compute_class_accuracy(
+                np.array(results["labels"]), np.array(results["preds"])
+            )
+            
+            print(f"validation loss: {average_loss:.5f}, accuracy: {accuracy * 100:2.2f}")
+            print("per-class accuracy: {}".format(class_accuracy))
+
+
+def compute_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+) -> float:
+    """
+    Args:
+        labels: ``(batch_size, class_count)`` tensor or array containing example labels
+        preds: ``(batch_size, class_count)`` tensor or array containing model prediction
+    """
+    assert len(labels) == len(preds)
+    return float((labels == preds).sum()) / len(labels)
+
+
+def compute_class_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+) -> np.array:
+
+    assert len(labels) == len(preds)
+    classes = np.zeros(10)
+    correct_classes = np.zeros(10)
+
+    a = 0
+    for class_number in labels:     
+        classes[class_number] = classes[class_number] + 1
+        if class_number == preds[a]:
+            correct_classes[class_number] = correct_classes[class_number] + 1
+        a = a + 1
+
+    result = correct_classes / classes * 100
+    return result
+
+
+def print_metrics(step, epoch, accuracy, loss, data_load_time, step_time, train_loader):
+        epoch_step = step % len(train_loader)
+        print(
+                f"epoch: [{epoch}], "
+                f"step: [{epoch_step}/{len(train_loader)}], "
+                f"batch loss: {loss:.5f}, "
+                f"batch accuracy: {accuracy * 100:2.2f}, "
+                f"data load time: "
+                f"{data_load_time:.5f}, "
+                f"step time: {step_time:.5f}"
+        )
 
 
 if __name__ == "__main__":
